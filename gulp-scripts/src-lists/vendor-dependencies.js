72c5d843d64dfea6265994ef4cfb7156ef8619f6
Ryan Kimber <ryan.kimber@gmail.com>
2014-08-25 13:25:10 -0400

    Brought header back. Added tiles. Starting to work a bit on content.

--- gulp-scripts/src-lists/vendor-dependencies.js
+++ gulp-scripts/src-lists/vendor-dependencies.js
@@ -2,5 +2,7 @@
 
 module.exports.directories = [
   './bower_components/bootstrap/dist/**/*',
-  './bower_components/jquery/dist/**/*'
+  './bower_components/jquery/dist/**/*',
+  './bower_components/fontawesome/css/**/*',
+  './bower_components/fontawesome/fonts/**/*'
 ];
